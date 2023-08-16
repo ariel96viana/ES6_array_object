c36628152f61b1cc1c6af1e16b93b80155b5e3ce
Ariel Oliveira <ariel96viana@gmail.com>
2023-08-16 12:49:59 -0300

    Refatorando o código para maior entendimento do código, aplicando melhores práticas

--- tarefa.js
+++ tarefa.js
@@ -7,30 +7,18 @@ alunos.forEach((nota, aluno) => {
   listaDeAlunos.push({ nome: aluno, nota: nota });
 });
 
-listaDeAlunos.forEach((aluno) => {
-  if (aluno.nota >= 6) {
-    console.log(`Parabéns ${aluno.nome}, você passou!`);
-  }
+const alunosAprovados = listaDeAlunos.filter((aluno) => aluno.nota >= 6);
+
+alunosAprovados.forEach((aluno) => {
+  console.log(
+    `Parabéns, ${aluno.nome}, você foi aprovado com nota: ${aluno.nota}`
+  );
 });
 
 function novoAluno() {
-  let nome = "Ariel";
-  let nota = "5";
-  alunos.set(nome, nota);
-
-  nome = "Mariana";
-  nota = "10";
-  alunos.set(nome, nota);
-
-  nome = "Evandro";
-  nota = "4";
-  alunos.set(nome, nota);
-
-  nome = "Nilza";
-  nota = "7";
-  alunos.set(nome, nota);
-
-  nome = "Nilza";
-  nota = "7";
-  alunos.set(nome, nota);
+  alunos.set("Ariel", "5");
+  alunos.set("Mariana", "10");
+  alunos.set("Evandro", "4");
+  alunos.set("Nilza", "7");
+  alunos.set("Nilza", "7");
 }
